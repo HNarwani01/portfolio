2df3e66d064eb1cb2f30723e47a2ce1eca8cb746
HNarwani01 <himanshurawat0075407@gmail.com>
2024-03-15 22:02:21 +0530

    links added

--- script/app.js
+++ script/app.js
@@ -10,12 +10,12 @@ let active = 'intro';
 let zIndex = 2;
 
 aHref.forEach(a => {
-    a.addEventListener('click', function(event){
+    a.addEventListener('click', function (event) {
         let tab = a.dataset.tab;
-        if(tab !== null && tab !== active){
+        if (tab !== null && tab !== active) {
 
             let activeOld = document.querySelector('.tab.active');
-            if(activeOld) activeOld.classList.remove('active');
+            if (activeOld) activeOld.classList.remove('active');
             active = tab;
 
             let tabActive = document.getElementById(active);
@@ -27,5 +27,40 @@ aHref.forEach(a => {
         }
     })
 })
+let imageElement = document.querySelectorAll('#project .container .list .item img');
+imageElement.forEach(element => {
+    element.addEventListener('click', () => {
+        let pageData = element.dataset.id;
+        switch (pageData) {
+            case 'pj-1':
+                window.open('https://hnarwani01.github.io/MAADHVIII-2.0/', '_blank');
+                break;
+            case 'pj-2':
+                window.open('http://shikshaseshikhartak.org/', '_blank');
+                break;
+            case 'pj-3':
 
+                window.open('http://shikshaseshikhartak.org/', '_blank');
+                break;
+            case 'pj-4':
+                window.open('http://shikshaseshikhartak.org/', '_blank');
+
+                break;
+            case 'pj-5':
+                window.open('http://shikshaseshikhartak.org/', '_blank');
+
+                break;
+            case 'pj-6':
+
+                window.open('http://shikshaseshikhartak.org/', '_blank');
+                break;
+
+            default:
+                break;
+        }
+    })
+})
 // https://hnarwani01.github.io/MAADHVIII-2.0/
+// http://shikshaseshikhartak.org/
+// https://hnarwani01.github.io/Js-projects/
+// https://hnarwani01.github.io/Frontend/
